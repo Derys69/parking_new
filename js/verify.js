08efcfd036a7573d937b162029dda07c3cfdfa86
Derys69 <ddharmawan03@studen.ciputra.ac.id>
2026-03-31 17:13:42 +0900

    tambah verify + redirect

---
+++ js/verify.js
@@ -0,0 +1,23 @@
+import { applyActionCode } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { auth } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js'; 
+
+const urlParams = new URLSearchParams(window.location.search);
+const oobCode = urlParams.get('oobCode');
+const mode = urlParams.get('mode');
+
+
+if (mode === 'verifyEmail' && oobCode) {
+    applyActionCode(auth, oobCode)
+        .then(() => {
+            alert("Email berhasil diverifikasi! Silakan login untuk melanjutkan.");
+
+            window.location.replace("login.html"); 
+        })
+        .catch((error) => {
+            alert("Gagal memverifikasi: Link mungkin sudah kadaluarsa atau tidak valid.");
+            window.location.replace("login.html");
+        });
+} else {
+
+    window.location.replace("login.html");
+}
